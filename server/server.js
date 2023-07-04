7e71966b6a7622b42b454cf7cdc792ccf570800e
KKKKKavie <kavienb@163.com>
2023-07-04 13:06:11 +0800

    deleteIcon and editIcon

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
     host: 'localhost',
     user: "root",
     password: "Dd20020429",
@@ -22,7 +22,7 @@ app.post('/api/register', (req, res) => {
     const username = req.body.username
     const password = req.body.password
     const sqlInsert = "INSERT INTO USER (username, password) VALUES (?, ?)"
-    db.query(sqlInsert, [username, password], (err, result)=>{
+    db.query(sqlInsert, [username, password], (err, result) => {
         console.log(err)
     })
 })
@@ -34,12 +34,12 @@ app.post('/api/login', (req, res) => {
     const username = req.body.username
     const password = req.body.password
     const sqlInsert = "SELECT * FROM USER WHERE username = ? AND password = ?"
-    db.query(sqlInsert, [username, password], (err, result)=>{
-      console.log(err)
+    db.query(sqlInsert, [username, password], (err, result) => {
+        console.log(err)
         if (result.length > 0) {
             res.send(result)
         } else {
-            res.send({message: "Wrong username/password combination!"})
+            res.send({ message: "Wrong username/password combination!" })
         }
     })
 })
@@ -47,35 +47,52 @@ app.post('/api/login', (req, res) => {
 app.post('/api/fetch_user', (req, res) => {
     // query username and password from database, returns query result
     const sqlInsert = "SELECT * FROM USER"
-    db.query(sqlInsert,(err, result)=>{
-      console.log(err)
+    db.query(sqlInsert, (err, result) => {
+        console.log(err)
         console.log(result)
         if (result.length > 0) {
             console.log(result)
             res.send(result)
         } else {
-            res.send({message: "No user exists!"})
+            res.send({ message: "No user exists!" })
+        }
+    })
+})
+
+app.post('/api/change_user_type', (req, res) => {
+    const username = req.body.username
+    const type = req.body.type
+
+    let newtype = "user"
+    if (type == "user") newtype = "admin"
+    const sqlInsert = "UPDATE USER SET type = ? WHERE username = ?"
+    db.query(sqlInsert, [newtype, username], (err, result) => {
+        console.log(err)
+        if (result.length > 0) {
+            res.send(result)
+        } else {
+            res.send({ message: "No user exists!" })
         }
     })
 })
 
 
 app.post('/api/rm_user', (req, res) => {
-//      This function is used by admin to remove user from database
+    //      This function is used by admin to remove user from database
     const username = req.body.username
     const sqlInsert = "DELETE FROM USER WHERE username = ?"
-    db.query(sqlInsert, [username], (err, result)=> {
+    db.query(sqlInsert, [username], (err, result) => {
         console.log(err)
         if (result.length > 0) {
             res.send(result)
         } else {
-            res.send({message: "No user exists!"})
+            res.send({ message: "No user exists!" })
         }
     })
 })
 
 app.post('/api/post_task', (req, res) => {
-//     this function is used by regular user to upload a task
+    //     this function is used by regular user to upload a task
     // task Content: task_name, task_description, task_deadline, task_status, task_priority, task_assignee, task_assigner
     const username = req.body.username
     const task_name = req.body.task_name
@@ -86,36 +103,36 @@ app.post('/api/post_task', (req, res) => {
     const task_assignee = req.body.task_assignee
     const task_assigner = req.body.task_assigner
     const sqlInsert = "INSERT INTO TASK (task_name, task_description, task_deadline, task_status, task_priority, task_assignee, task_assigner) VALUES (?, ?, ?, ?, ?, ?, ?)"
-    db.query(sqlInsert, [task_name, task_description, task_deadline, task_status, task_priority, task_assignee, task_assigner], (err, result)=>{
+    db.query(sqlInsert, [task_name, task_description, task_deadline, task_status, task_priority, task_assignee, task_assigner], (err, result) => {
         console.log(err)
-        if(result.length > 0) {
+        if (result.length > 0) {
             res.send(result)
         }
         else {
-            res.send({message: "No user exists!"})
+            res.send({ message: "No user exists!" })
         }
     })
 })
 
 app.post('/api/rm_task', (req, res) => {
-//     this function is used by regular user to remove a task, authentication and validation is done by previously defined database triggers.
+    //     this function is used by regular user to remove a task, authentication and validation is done by previously defined database triggers.
     const task_name = req.body.task_name
     const sqlInsert = "DELETE FROM TASK WHERE task_name = ?"
-    db.query(sqlInsert, [task_name], (err, result)=> {
+    db.query(sqlInsert, [task_name], (err, result) => {
         console.log(err)
         if (result.length > 0) {
             res.send(result)
         } else {
-            res.send({message: "No task exists!"})
+            res.send({ message: "No task exists!" })
         }
     })
 })
 
 
 app.post('/api/update_task', (req, res) => {
-//     update some of the task content, authentication and validation is done by previously defined database triggers.
-//     the content to be modified is given by a json object, which contains the task_name and the content to be modified.
-//     task_name is
+    //     update some of the task content, authentication and validation is done by previously defined database triggers.
+    //     the content to be modified is given by a json object, which contains the task_name and the content to be modified.
+    //     task_name is
 })
 
 
